b7cf3a1c483a4fc39ce2ee4d96632e77f4565172
Anwesh43 <anweshthecool0@gmail.com>
2019-12-22 17:13:36 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsweepmulticolorview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsweepmulticolorview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsweepmulticolorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.sweepmulticolorview.SweepMultiColorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SweepMultiColorView.create(this)
     }
 }
