8028106b72b32af39fda9d10f9a03a8234bf45de
Anwesh43 <anweshthecool0@gmail.com>
2019-12-22 16:33:37 +0530

    adding SweepMultiColorView class

--- sweepmulticolorview/src/main/java/com/anwesh/uiprojects/sweepmulticolorview/SweepMultiColorView.kt
+++ sweepmulticolorview/src/main/java/com/anwesh/uiprojects/sweepmulticolorview/SweepMultiColorView.kt
@@ -63,3 +63,21 @@ fun Canvas.drawSMCNode(i : Int, scale : Float, paint : Paint) {
     restore()
 }
 
+class SweepMultiColorView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
+
