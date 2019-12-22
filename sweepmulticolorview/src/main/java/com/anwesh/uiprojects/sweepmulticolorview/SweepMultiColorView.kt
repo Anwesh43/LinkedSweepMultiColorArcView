0c208ef85c2ce9112ffedf0d054f8ca3f5e40afb
Anwesh43 <anweshthecool0@gmail.com>
2019-12-22 17:02:58 +0530

    adding SMCNode class

--- sweepmulticolorview/src/main/java/com/anwesh/uiprojects/sweepmulticolorview/SweepMultiColorView.kt
+++ sweepmulticolorview/src/main/java/com/anwesh/uiprojects/sweepmulticolorview/SweepMultiColorView.kt
@@ -127,5 +127,48 @@ class SweepMultiColorView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+
+    data class SMCNode(var i : Int, val state : State = State()) {
+
+        private var next : SMCNode? = null
+        private var prev : SMCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SMCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSMCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SMCNode {
+            var curr : SMCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
 
